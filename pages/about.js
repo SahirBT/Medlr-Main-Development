c683e991152f11953f5ec18092547d50f6fae15d
YuvrajBT <yuvraj@bthustlers.com>
2023-03-27 23:03:34 +0530

    Changed Background Color

--- pages/about.js
+++ pages/about.js
@@ -7,7 +7,17 @@ export default function about() {
         <title>About</title>
         <meta name='keywords' content='medicine search'/>
       </Head>
-			<h1>About</h1>
+			{/* <h1>About</h1>
+       */}
+       <div className="absolute h-max  left-0 bg-gray-300">
+         <h1>About Medler</h1>
+         <span>
+          One-Stop Shop For Easy Comparison  <br />
+          Shopping And Negotiations For  <br />
+          Prescription Medicines 
+         </span>
+
+       </div>
     </div>
   );
 }
--- pages/resetpassword.js
+++ pages/resetpassword.js
@@ -89,7 +89,7 @@ const resetValidate = (values) => {
                   type="password"
                   autoComplete="current-password"
                   required
-                  className="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-[#00BFBE] sm:text-sm sm:leading-6"
+                  className="relative block w-full rounded-b-md border-0 py-1.5 px-16 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-[#00BFBE] sm:text-sm sm:leading-6"
                   placeholder="Password"
                 />
               </div>
@@ -103,7 +103,7 @@ const resetValidate = (values) => {
                   type="password"
                   autoComplete="current-password"
                   required
-                  className="relative block w-full rounded-b-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-[#00BFBE] sm:text-sm sm:leading-6"
+                  className="relative block w-full rounded-b-md border-0 py-1.5 px-16 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-[#00BFBE] sm:text-sm sm:leading-6"
                   placeholder="Confirm Password"
                 />
               </div>
