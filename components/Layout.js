586c87369a46919a34920d6418dc2c1c668e144f
SkaiLord <skailord007@gmail.com>
2023-04-27 21:09:17 +0530

    first commit

--- components/Layout.js
+++ components/Layout.js
@@ -4,13 +4,13 @@ import Footer from './Footer';
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <div className='w-full'>
       <Navbar />
       <div className="bg-[#ffffff] w-full p-8 overflow-hidden">
         <div className={styles.main}>{children}</div>
       </div>
       <Footer />
-    </>
+    </div>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,28 @@ module.exports = {
   "./node_modules/tw-elements/dist/js/**/*.js"],
   theme: {
     extend: {},
+    screens: {
+      '2xs':'320px',
+      // => @media (min-width: 320px) { ... }
+
+      'xs':'425px',
+      // => @media (min-width: 425px) { ... }
+
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    }
   },
   plugins: [
     require('@tailwindcss/forms'),
